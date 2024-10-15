8a3fc7879a56401a85a2119640ac687670b594b5
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-10-15 07:09:22 -0300

    Clase 2: Creación de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Articulo from "./Articulo";
+
+function App() {
+  return (
+    <>
+      <h1>Hola Camada 3!!</h1>
+      <p>Este es mi primer proyecto</p>
+      <ul>
+        <li>Elemento 1</li>
+        <li>Elemento 2</li>
+        <li>Elemento 3</li>
+        <li>Elemento 4</li>
+        <li>Elemento 5</li>
+      </ul>
+      <Articulo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Articulo.jsx
@@ -0,0 +1,9 @@
+const Articulo = () => {
+  return (
+    <article>
+      <h4>Usuario: Julián Debrabandere</h4>
+      <p>Rol: Docente</p>
+    </article>
+  );
+};
+export default Articulo;
