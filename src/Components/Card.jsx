83645299aad73eaab23c727a90d8d8d916a269cf
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-11-13 22:18:00 -0300

    Clase 10: Fetch y Axios

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import CardStyles from "../styles/Card.module.css";
 import Counter from "./Counter";
 
-const Card = ({ pizza, cart, setCart }) => {
-  const { tipo, img, precio } = pizza;
+const Card = ({ recipe, cart, setCart }) => {
+  const { title, image, pricePerServing } = recipe;
   const [counter, setCounter] = useState(0);
   return (
     <div className={CardStyles.cardContainer}>
-      <img src={img} alt={tipo} />
-      <h3>{tipo}</h3>
-      <h4>{precio}</h4>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <h4>${pricePerServing}</h4>
       <Counter counter={counter} setCounter={setCounter} />
       <button
         disabled={counter === 0}
-        onClick={() => setCart([...cart, pizza])}
+        onClick={() => setCart([...cart, recipe])}
       >
         Agregar al 🛒
       </button>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { pizzas } from "../utils/pizzas";
 import Card from "./Card";
+import axios from "axios";
 
 const Home = () => {
   const [cart, setCart] = useState([]);
+  const [recipes, setRecipes] = useState([]);
   console.log(cart);
+  const url = `https://api.spoonacular.com/recipes/random?number=10&apiKey=${
+    import.meta.env.VITE_API_KEY
+  }`;
+  useEffect(() => {
+    axios(url).then((res) => {
+      console.log(res.data.recipes);
+      setRecipes(res.data.recipes);
+    });
+  }, []);
+
   return (
     <div className="list-container">
-      {pizzas.map((pizza) => (
-        <Card key={pizza.id} pizza={pizza} cart={cart} setCart={setCart} />
+      {recipes.map((recipe) => (
+        <Card key={recipe.id} recipe={recipe} cart={cart} setCart={setCart} />
       ))}
     </div>
   );
